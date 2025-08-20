44616bf0da06f9af2ef35919a7e50e34422a3d07
CNayana17 <167953574+CNayana17@users.noreply.github.com>
2025-08-20 20:26:19 +0530

    Add files via upload

---
+++ test/src/test/MajorityElement.java
@@ -0,0 +1,17 @@
+package aug_20;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] nums = {2,2,1,1,1,2,2};
+        System.out.println("Majority Element: " + majorityElement(nums));
+    }
+
+    public static int majorityElement(int[] nums) {
+        int count = 0, candidate = 0;
+        for (int num : nums) {
+            if (count == 0) candidate = num;
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+}
