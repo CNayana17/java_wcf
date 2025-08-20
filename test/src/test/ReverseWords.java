b52766b429f8dd20ab71993fe80eadabb91fcfaf
CNayana17 <167953574+CNayana17@users.noreply.github.com>
2025-08-20 20:29:34 +0530

    Add files via upload

---
+++ test/src/test/ReverseWords.java
@@ -0,0 +1,15 @@
+package aug_20;
+
+public class ReverseWords {
+    public static void main(String[] args) {
+        String str = "Hello World Java";
+        String[] words = str.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (String word : words) {
+            result.append(new StringBuilder(word).reverse()).append(" ");
+        }
+
+        System.out.println("Reversed words: " + result.toString().trim());
+    }
+}
